8d6ec61a14c8855ac371c4ffed5161eef0bc1d0e
jooedvard <jooedvard7@gmail.com>
2022-06-06 19:48:33 +0200

    marker find address

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-$(() => {main()});
+$(() => {
+  main();
+});
 
 function main() {
-  
-    getInfoFromIP();
-    getUserLocation((location) => {
-      console.log(location);
+  getInfoFromIP();
+  getUserLocation((location) => {
+    console.log(location);
     let map = document.querySelector("#map");
     let { latitude, longitude } = location.coords;
     createMap(map, latitude, longitude);
-    
   });
 }
 
@@ -23,28 +23,56 @@ function getUserLocation(callback) {
 function createMap(domElement, lat, long) {
   let map = L.map(domElement).setView([lat, long], 13);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-
+  mapClick(map);
 }
 
-function addMarker(map,lat,long){
-    L.marker([lat,long]).addTo(map);
+function addMarker(map, lat, long) {
+  let marker = L.marker([lat, long]).addTo(map);
+  return marker;
 }
 
-async function fetchData(url,callback){
-    let request = await fetch(url);
-    let response = await request.json().then((data)=>{
-        callback(data);
-    });
-    
+function mapClick(map) {
+  map.on("click", (event) => {
+    let { lat, lng } = event.latlng;
+    let marker = addMarker(map, lat, lng);
+    findStreet(lat, lng, marker);
+  });
 }
 
-async function getInfoFromIP(){
-    let userinfo = $(".userinfo");
-    let apikey = "e9dbdc6656b821659d74c53310cf97e3904c719ca9014eca74c572bc";
-    let url = "https://api.ipdata.co?api-key="+apikey;
-    fetchData(url,(data)=>{
-        let info = new Userinfo(data,userinfo);
-        info.showData();
-    })
+function findStreet(lat, long,marker) {
+  let api = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${long}`;
+  fetchData(api, (street) => {
+    console.log(street);
+        let {country,postcode,city_district,shop,industrial,road,suburb,type} = street.address;
+        marker.bindPopup(`
+        <div>
+          <span>${country}</span>
+          <span>${postcode}</span>
+          <span>${city_district}</span>
+          <span>${road}</span>
+        </div>
+        <div>
+        <span>${suburb}</span>
+        <span> ${shop=== undefined ? "":shop} ${industrial === undefined ? "":industrial}</span>
+        <span>${type === undefined ? "":type}</span>
+        </div>
+        `);
+  });
 }
 
+async function fetchData(url, callback) {
+  let request = await fetch(url);
+  let response = await request.json().then((data) => {
+    callback(data);
+  });
+}
+
+async function getInfoFromIP() {
+  let userinfo = $(".userinfo");
+  let apikey = "e9dbdc6656b821659d74c53310cf97e3904c719ca9014eca74c572bc";
+  let url = "https://api.ipdata.co?api-key=" + apikey;
+  fetchData(url, (data) => {
+    let info = new Userinfo(data, userinfo);
+    info.showData();
+  });
+}
