341cc149ee548ce8951e9ce42b0898b6815789f9
jooedvard <jooedvard7@gmail.com>
2022-06-07 21:04:42 +0200

    error handling

--- classes/Location.js
+++ classes/Location.js
@@ -4,36 +4,60 @@ class Locations {
   }
 
   add(location) {
+    let location1 = $("#location1");
+    let location2 = $("#location2");
+
     this.locations.push(location);
     if (this.locations.length == 2) {
+      location2.val(location.data.properties.label);
       this.createPlan();
+     
+    }
+    else{
+      location1.val(location.data.properties.label);
     }
   }
 
   createPlan() {
     let road = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248f5aeefad36a74b7fbc0428cf783e8f21&start=${this.locations[0].coordinates[0]},${this.locations[0].coordinates[1]}&end=${this.locations[1].coordinates[0]},${this.locations[1].coordinates[1]}`;
-    fetchData(road, (path) => {
-      let map = this.locations[0].map;
-      let { coordinates } = path.features[0].geometry;
+    fetchData(
+      road,
+      (path) => {
+        let map = this.locations[0].map;
+        let { coordinates } = path.features[0].geometry;
 
-      L.Routing.control({
-        waypoints: [
-          L.latLng(coordinates[0][1], coordinates[0][0]),
-          L.latLng(
-            coordinates[coordinates.length - 1][1],
-            coordinates[coordinates.length - 1][0]
-          ),
-        ],
-        lineOptions: {
-          styles: [{ color: "blue" }],
-        },
-      }).addTo(map);
-      this.locations = [];
-      let search = $(".search");
-      let locations = $(".locations");
-      search.val('');
-      locations.slideUp(250);
-    });
+        L.Routing.control({
+          waypoints: [
+            L.latLng(coordinates[0][1], coordinates[0][0]),
+            L.latLng(
+              coordinates[coordinates.length - 1][1],
+              coordinates[coordinates.length - 1][0]
+            ),
+          ],
+          lineOptions: {
+            styles: [{ color: "blue" }],
+          },
+        }).addTo(map);
+
+        this.locations = [];
+        this.hideLocationDomElements();
+      },
+      () => {
+        this.locations = [];
+        this.hideLocationDomElements();
+      }
+    );
+  }
+
+  hideLocationDomElements() {
+    let location1 = $("#location1");
+    let location2 = $("#location2");
+    let search = $(".search");
+    let locations = $(".locations");
+    location1.val('');
+    location2.val('');
+    search.val("");
+    locations.slideUp(250);
   }
 }
 
@@ -51,5 +75,4 @@ class Location {
       window.dispatchEvent(event);
     });
   }
-
 }
--- index.js
+++ index.js
@@ -10,56 +10,55 @@ function main() {
     createMap(map, latitude, longitude);
     onAddLocations();
   });
-
-
 }
 function createMap(domElement, lat, long) {
   let map = L.map(domElement).setView([lat, long], 10);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
   autoCompleteLocation(map);
-  
 }
-
 
 function autoCompleteLocation(map) {
   let api = `https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf6248f5aeefad36a74b7fbc0428cf783e8f21&text=`;
   let search = $(".search");
   let locations = $(".locations");
- 
+
   search.on("input", (event) => {
     locations.hide();
     value = event.target.value;
     if (value !== "" || "") {
       fetchData(api + value, (data) => {
         locations.empty();
-          let {features} = data;
-          features.forEach(location=>{
-              new Location(locations,location,map);
-          })
-          locations.slideDown(250);
+        let { features } = data;
+        features.forEach((location) => {
+          new Location(locations, location, map);
+        });
+        locations.slideDown(250);
       });
-    }
-    else{
+    } else {
       locations.empty();
     }
   });
 }
 
-function onAddLocations(){
+function onAddLocations() {
   let places = new Locations();
-  $(window).on("addLocations",(event)=>{
-      places.add(event.detail);
-  })
-
-}
-
-async function fetchData(url, callback) {
-  let request = await fetch(url);
-  let response = await request.json().then((data) => {
-    callback(data);
+  $(window).on("addLocations", (event) => {
+    places.add(event.detail);
   });
 }
 
+async function fetchData(url, callback,onerror) {
+  await fetch(url).then((res) => {
+    if (res.ok) {
+      res.json().then((data) => {
+        callback(data);
+      });
+    } else {
+      alert("Sajnos nem találtunk útvonalat a rendszerünkben!");
+      onerror();
+    }
+  });
+}
 
 async function getInfoFromIP() {
   let userinfo = $(".userinfo");
@@ -78,4 +77,3 @@ function getUserLocation(callback) {
     });
   }
 }
-
